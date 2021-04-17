805cf6d8c99e4baef20f02a9620a4dd7ee969624
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-04-17 18:28:52 -0300

    config

---
+++ src/test/java/com/dev22/aldoapi/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev22.aldoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
